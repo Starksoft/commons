3a21ec1c6b24f5ec683489a392cb3c6403bf399b
Anisonyan Edward <4238662+Starksoft@users.noreply.github.com>
2019-10-06 13:00:35 +0300

    Version 1.0.2 ships to jcenter

--- commons/src/main/java/ru/starksoft/commons/extensions/TextInputLayoutExtensions.kt
+++ commons/src/main/java/ru/starksoft/commons/extensions/TextInputLayoutExtensions.kt
@@ -10,34 +10,58 @@ private const val TAG = "TextInputLayoutExtensions"
 
 @SuppressLint("LongLogTag")
 fun TextInputLayout.getTextSafe(): String {
-    val editText = editText
-    if (editText !is TextInputEditText) {
-        Log.d(
-            TAG,
-            "getTextSafe: Edit text is not a valid TextInputEditText class, consider to use TextInputEditText as a child of TextInputLayout"
-        )
-    }
+	val editText = editText
+	if (editText !is TextInputEditText) {
+		Log.d(
+			TAG,
+			"getTextSafe: Edit text is not a valid TextInputEditText class, consider to use TextInputEditText as a child of TextInputLayout"
+		)
+	}
 
-    return if (editText == null || editText.text == null) {
-        ""
-    } else editText.text.trim().toString()
+	return if (editText == null || editText.text == null) {
+		""
+	} else editText.text.trim().toString()
 }
 
 fun TextInputLayout?.resetError() {
-    val textInputLayout = this
+	val textInputLayout = this
 
-    //textInputLayout?.error?.isNotEmpty() == true
-    if (textInputLayout != null && !TextUtils.isEmpty(textInputLayout.error)) {
-        textInputLayout.error = null
-        // Хак, чтобы убрать отступы у TextInputLayout
-        textInputLayout.isErrorEnabled = false
-    }
+	//textInputLayout?.error?.isNotEmpty() == true
+	if (textInputLayout != null && !TextUtils.isEmpty(textInputLayout.error)) {
+		textInputLayout.error = null
+		// Хак, чтобы убрать отступы у TextInputLayout
+		textInputLayout.isErrorEnabled = false
+	}
 }
 
 fun TextInputLayout.setTextWithTrim(input: String?): String {
-    return input?.trim()?.let {
-        editText?.setText(it)
-        editText?.setSelection(it.length)
-        it
-    } ?: ""
+	return input?.trim()?.let {
+		editText?.setText(it)
+		editText?.setSelection(it.length)
+		it
+	} ?: ""
+}
+
+fun TextInputLayout.validateEmpty(): Boolean {
+
+	val text = this.editText?.text
+
+	return if (text.isNullOrBlank()) {
+		this.error = "Поле не должно быть пустым или содержать только пробелы"
+		false
+	} else {
+		true
+	}
+}
+
+fun TextInputLayout.validateRegex(regex: Regex): Boolean {
+
+	val text = this.editText?.text.toString()
+
+	return if (!regex.matches(text)) {
+		this.error = "Недопустимый ввод"
+		false
+	} else {
+		true
+	}
 }
